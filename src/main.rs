d642b70b3393f4f049fcfb624761e0b3227bbacc
Sklz-PC <sliperly@gmail.com>
2024-01-29 10:25:40 +0300

    Отредактирован файл main.rs по результатам третьего урока. Используем крейт clap.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 mod editor;
 
+use clap::{Command, Arg};
+
+const APP_NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const FILE_PATH_ARG: &str = "file"
+
 use editor::Editor;
 
-fn  main() -> Result<(), std::io::Error> {
-    let mut editor: Editor = Editor::new()?;
-    editor.run()
-}
-use std::io::{self, Read, Write};
-use std::io::{Stdin, Bytes};
-use termion::raw::RawTerminal;
-use termion::raw::IntoRawMode;
+fn  main() {
+    let matches = Command::new(APP_NAME)
+        .version(VERSION)
+        .arg(
+            Arg::new(FILE_PATH_ARG)
+                .required(true)
+                .index(1)
+        )
+        .get_matches();
 
+    let _file_path = matches.get_one::<String>(FILE_PATH_ARG).unwrap();
+    let mut editor = editor::Editor::new().unwrap();
+    editor.run().unwrap():
+}
